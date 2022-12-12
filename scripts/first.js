7a5ae6432983eed696d4d690df7981f786074e45
swetha <swethaskr6143@gmail.com>
2022-12-12 13:21:48 +0530

    first

---
+++ scripts/first.js
@@ -0,0 +1,80 @@
+// functions
+// function learnMessage(smile) {
+//     console.log(smile);
+// }
+//    function sub(x,y){
+//     let SubofNumbers= x-y;
+//     return SubofNumbers;
+//    }
+//    let result= sub(6,5);
+//    learnMessage(result);
+
+//function learnmessage(happy) {
+   // console.log(happy);
+// }
+//    function mul(x,y){
+//     return x*y;
+//    }
+//    let result= mul(6,5);
+//    learnmessage(result);
+      //in2cm&cmin
+   function inToCm(x){
+    return x*2.54;
+   }
+   const in2cm=inToCm(5);
+   console.log(in2cm+'cm');
+
+//    function cmToIn(y)
+//    {
+//     return y%2.54;
+//    }
+   
+//    const result=cmToIn(5);
+//    console.log(result);
+
+// // kg2gram&gram2kg
+// function kgToGram(z){
+//     return z*1000;
+// }
+// const result=kgToGram(6);
+// console.log(result);
+
+// function gramToKg(m){
+//     return m*1/1000;
+// }
+// const result=gramToKg(6);
+// console.log(result)
+
+// /mm2cm&cm2mm
+
+// function cmToMm(N){
+//     return N*100;
+// }
+// const result=cmToMm(30);
+// console.log(result)
+
+// function mmToCm(P){
+//     return P*1/100;
+// }
+// const result=mmToCm(30);
+// console.log(result)
+
+
+
+// let value = "goood morning"
+// result=value[0].toUpperCase();
+// console.log(result);
+
+// function capitalize(word){
+//     let lowerWord=word.tolowerCase();
+//     return word[0].toUpperCase()+lowerWord.slice(1);
+//  }
+//  let str="web developer";
+//  str.split(" ").forEach(word=>console.log(capitalize(word)));
+
+let fruit={
+   name:"mango",
+   season:"summer",
+
+};
+console.log(fruit.name);
